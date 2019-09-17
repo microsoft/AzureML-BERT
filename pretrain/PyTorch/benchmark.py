ccd4d8e3b9f951f4e9270a623bf9c482771c9dab
Aashna Garg <aashna@users.noreply.github.com>
2019-09-17 09:47:06 -0700

    Updating calculations following PR comments

--- pretrain/PyTorch/benchmark.py
+++ pretrain/PyTorch/benchmark.py
@@ -13,6 +13,7 @@ def get_perf_metrics(filename):
 		throughput = 0
 		epoch = 1
 		time_diff=0
+		num_seq=0
 		for line in datafile:
 			if 'Training epoch:' in line:
 				start_time = get_timestamp(line)
@@ -22,12 +23,13 @@ def get_perf_metrics(filename):
 				epoch += 1
 			if 'Completed processing' in line:
 				end_time = get_timestamp(line)
-				time_diff = int((end_time-start_time).total_seconds())
-				num_seq = [int(s) for s in line[int(line.find('Completed processing')):].split() if s.isdigit()][0]
-				throughput += num_seq/time_diff
+				time_diff += int((end_time-start_time).total_seconds())
+				num_seq += [int(s) for s in line[int(line.find('Completed processing')):].split() if s.isdigit()][0]
+				throughput = num_seq/time_diff
+				#print(throughput)
 			if 'Validation Loss' in line:
 				valid_loss = float(line[int(line.find('is:'))+3:])
-		avg_throughput = throughput/float(epoch)
+		avg_throughput = (num_seq/time_diff)
 		total_training_time = end_time-training_start_time
 		d = datetime(1,1,1) + total_training_time
 
