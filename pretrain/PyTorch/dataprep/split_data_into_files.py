8ae76e7becfc77dbbaa8997666a2d74715575a0a
Balázs Indig <indba@digitus.itk.ppke.hu>
2020-05-22 21:09:04 +0200

    Refactor and fix splitting to shards

--- pretrain/PyTorch/dataprep/split_data_into_files.py
+++ pretrain/PyTorch/dataprep/split_data_into_files.py
@@ -1,41 +1,32 @@
-import os
 from tqdm import tqdm
 
-input_file = 'wikipedia.segmented.nltk.txt'
-output_file = './data_shards/wikipedia.segmented.part.'
+input_file = "wikipedia.segmented.nltk.txt"
+output_file = "./data_shards/wikipedia.segmented.part."
 
 doc_seperator = "\n"
-
-line_buffer = []
 total_partitions = 100  # Mostly will create 1 extra partition
 # shard_size = 396000 # Approximate, will split at next article break
-line_counter = 0
-shard_index = 0
 
-ifile_lines = 0
-with open(input_file) as ifile:
-    for i, line in tqdm(enumerate(ifile)):
-        ifile_lines += 1
+with open(input_file, encoding="UTF-8") as ifile:
+    ifile_lines = sum(1 for _ in tqdm(ifile))
 
 print("Input file contains", ifile_lines, "lines.")
 
 shard_size = ifile_lines // total_partitions
 
-iline_counter = 1
-with open(input_file) as ifile:
-    for i, line in tqdm(enumerate(ifile)):
-        if line_counter < shard_size and iline_counter < ifile_lines:
-            line_buffer.append(line)
+with open(input_file, encoding="UTF-8") as ifile:
+    line_counter = 0
+    shard_index = 0
+    ofile = open(f"{output_file}{shard_index}.txt", "w", encoding="UTF-8")  # Open the first file
+    for line in tqdm(ifile):
+        # We take whole documents and write at least shard_size lines into a file
+        if line_counter < shard_size or (line_counter >= shard_size and line != doc_seperator):
+            ofile.write(line)
             line_counter += 1
-            iline_counter += 1
-        elif line_counter >= shard_size and line != "\n" and iline_counter < ifile_lines:
-            line_buffer.append(line)
-            line_counter += 1
-            iline_counter += 1
         else:
-            with open(output_file + str(shard_index) + ".txt", "w") as ofile:
-                for oline in line_buffer:
-                    ofile.write(oline)
-                line_buffer = []
-                line_counter = 0
-                shard_index += 1
+            line_counter = 0
+            shard_index += 1
+            ofile.close()
+            ofile = open(f"{output_file}{shard_index}.txt", "w", encoding="UTF-8")
+            ofile.write(line)  # Do not forget to write this line too!
+    ofile.close()  # Close the lastfile
