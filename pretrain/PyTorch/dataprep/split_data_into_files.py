4b27fac863ed7ca8c7974dbe3a7c6f4f9dbe12e9
Indig Balázs <dlazesz@users.noreply.github.com>
2020-06-24 20:29:40 +0200

    Refactor and fix splitting to shards (#56)
    * Refactor and fix splitting to shards
    
    * Fix previous commit
    
    * Review: Rename line_counter to shard_line_counter and add some comment

--- pretrain/PyTorch/dataprep/split_data_into_files.py
+++ pretrain/PyTorch/dataprep/split_data_into_files.py
@@ -1,41 +1,33 @@
-import os
 from tqdm import tqdm
 
-input_file = 'wikipedia.segmented.nltk.txt'
-output_file = './data_shards/wikipedia.segmented.part.'
+input_file = "wikipedia.segmented.nltk.txt"
+output_file = "./data_shards/wikipedia.segmented.part."
 
 doc_seperator = "\n"
-
-line_buffer = []
 total_partitions = 100  # Mostly will create 1 extra partition
 # shard_size = 396000 # Approximate, will split at next article break
-line_counter = 0
-shard_index = 0
 
-ifile_lines = 0
-with open(input_file) as ifile:
-    for i, line in tqdm(enumerate(ifile)):
-        ifile_lines += 1
+with open(input_file, encoding="UTF-8") as ifile:
+    ifile_lines = sum(1 for _ in tqdm(ifile))
 
 print("Input file contains", ifile_lines, "lines.")
 
 shard_size = ifile_lines // total_partitions
 
-iline_counter = 1
-with open(input_file) as ifile:
-    for i, line in tqdm(enumerate(ifile)):
-        if line_counter < shard_size and iline_counter < ifile_lines:
-            line_buffer.append(line)
-            line_counter += 1
-            iline_counter += 1
-        elif line_counter >= shard_size and line != "\n" and iline_counter < ifile_lines:
-            line_buffer.append(line)
-            line_counter += 1
-            iline_counter += 1
-        else:
-            with open(output_file + str(shard_index) + ".txt", "w") as ofile:
-                for oline in line_buffer:
-                    ofile.write(oline)
-                line_buffer = []
-                line_counter = 0
-                shard_index += 1
+with open(input_file, encoding="UTF-8") as ifile:
+    shard_line_counter = 0
+    shard_index = 0
+    ofile = open(f"{output_file}{shard_index}.txt", "w", encoding="UTF-8")  # Open the first file
+    # Output files should not have doc_separator at the end of the file, but we accept input ending with doc_separator
+    for iline_counter, line in tqdm(enumerate(ifile, start=1)):
+        if line != doc_seperator or shard_line_counter < shard_size:
+            shard_line_counter += 1
+            ofile.write(line)
+        # Prevent opening an empty output file or writing a doc_sep
+        # when the iteration has reached the end of the input file (iline_counter == ifile_lines)
+        elif iline_counter < ifile_lines:
+            shard_line_counter = 0
+            shard_index += 1
+            ofile.close()
+            ofile = open(f"{output_file}{shard_index}.txt", "w", encoding="UTF-8")
+    ofile.close()  # Close the lastfile
